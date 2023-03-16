a5099607b0ef4466765e8b3c8d93f42970900958
MarkMurasaki <mark.murasaki.chan@gmail.com>
2023-03-16 13:20:26 +0800

    qwe

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -40,6 +41,8 @@ app.get("/api/:timestamp",function (req,res){
     res.json({"unix": new Date(nd).getTime(),"utc": new Date(nd).toUTCString() });
   }
 });
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
